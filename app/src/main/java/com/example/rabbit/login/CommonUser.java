d5545b78de4e2b7e711f4603d98ba87cb0925c08
Cofitost <zxcv091922@gmail.com>
2017-02-09 23:14:15 +0800

    http改用httpclient

--- app/src/main/java/com/example/rabbit/login/CommonUser.java
+++ app/src/main/java/com/example/rabbit/login/CommonUser.java
@@ -4,15 +4,33 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.view.View.OnClickListener;
 import android.widget.EditText;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CommonUser extends AppCompatActivity {
 
-    EditText accountNumber,username,password,email,phone;
+    EditText accountNumber,userName,password,email,phone;
     Button done,cancel;
 
     @Override
@@ -23,7 +41,7 @@ public class CommonUser extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         accountNumber = (EditText)findViewById(R.id.ET_signUp_accountNumber);
-        username = (EditText)findViewById(R.id.ET_signUp_name);
+        userName = (EditText)findViewById(R.id.ET_signUp_name);
         password = (EditText)findViewById(R.id.ET_signUp_PWD);
         email = (EditText)findViewById(R.id.ET_signUp_email);
         phone = (EditText)findViewById(R.id.ET_signUp_phone);
@@ -39,8 +57,7 @@ public class CommonUser extends AppCompatActivity {
         @Override
         public void onClick(View v) {
 
-            PostCommon post = new PostCommon();
-            post.onPost();
+            onPost();
 
             Intent intent = new Intent();
             intent.setClass(CommonUser.this,MainActivity.class);
@@ -58,6 +75,53 @@ public class CommonUser extends AppCompatActivity {
             finish();
         }
     };
+
+    public void onPost(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                httpPost();
+            }
+        }).start();
+    }
+
+    public void httpPost(){
+        String result = null;
+
+        HttpClient client = new DefaultHttpClient();
+        try {
+            HttpPost post = new HttpPost("http://192.168.2.59:8080/android-backend/webapi/food/add");
+            List<NameValuePair> params = new ArrayList<NameValuePair>();
+            //params.add(new BasicNameValuePair("key",value));
+            //params.add(new BasicNameValuePair("hour",postHour));
+            params.add(new BasicNameValuePair("accountNumber",accountNumber.getText().toString()));
+            params.add(new BasicNameValuePair("userName",userName.getText().toString()));
+            params.add(new BasicNameValuePair("password",password.getText().toString()));
+            params.add(new BasicNameValuePair("email",email.getText().toString()));
+            params.add(new BasicNameValuePair("phone",phone.getText().toString()));
+
+            UrlEncodedFormEntity ent = null;
+            Log.d("abc",params.toString());
+
+            ent = new UrlEncodedFormEntity(params, HTTP.UTF_8);
+            post.setEntity(ent);
+            HttpResponse responsePOST = client.execute(post);
+            HttpEntity resEntity = responsePOST.getEntity();
+
+            if (resEntity != null) {
+                result = EntityUtils.toString(resEntity);
+                Log.d("abcd",result);
+            }
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            client.getConnectionManager().shutdown();
+        }
+    }
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event)
--- app/src/main/java/com/example/rabbit/login/SeminarOrganizers.java
+++ app/src/main/java/com/example/rabbit/login/SeminarOrganizers.java
@@ -6,15 +6,33 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class SeminarOrganizers extends AppCompatActivity {
 
-    EditText accountNumber,username,password,principal,email,phone;
+    EditText accountNumber,userName,password,principal,email,phone;
     Button done,cancel;
 
     @Override
@@ -25,7 +43,7 @@ public class SeminarOrganizers extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         accountNumber = (EditText)findViewById(R.id.ET_organizersignUp_accountNumber);
-        username = (EditText)findViewById(R.id.ET_organizersignUp_name);
+        userName = (EditText)findViewById(R.id.ET_organizersignUp_name);
         password = (EditText)findViewById(R.id.ET_organizersignUp_PWD);
         email = (EditText)findViewById(R.id.ET_organizersignUp_email);
         phone = (EditText)findViewById(R.id.ET_organizersignUp_phone);
@@ -42,8 +60,7 @@ public class SeminarOrganizers extends AppCompatActivity {
         @Override
         public void onClick(View v) {
 
-            PostOrganizers post = new PostOrganizers();
-            post.onPost();
+            onPost();
 
             Intent intent = new Intent();
             intent.setClass(SeminarOrganizers.this,MainActivity.class);
@@ -61,6 +78,54 @@ public class SeminarOrganizers extends AppCompatActivity {
             finish();
         }
     };
+
+    public void onPost(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                httpPost();
+            }
+        }).start();
+    }
+
+    public void httpPost(){
+        String result = null;
+
+        HttpClient client = new DefaultHttpClient();
+        try {
+            HttpPost post = new HttpPost("http://192.168.2.59:8080/android-backend/webapi/food/add");
+            List<NameValuePair> params = new ArrayList<NameValuePair>();
+            //params.add(new BasicNameValuePair("key",value));
+            //params.add(new BasicNameValuePair("hour",postHour));
+            params.add(new BasicNameValuePair("accountNumber",accountNumber.getText().toString()));
+            params.add(new BasicNameValuePair("userName",userName.getText().toString()));
+            params.add(new BasicNameValuePair("password",password.getText().toString()));
+            params.add(new BasicNameValuePair("email",email.getText().toString()));
+            params.add(new BasicNameValuePair("phone",phone.getText().toString()));
+            params.add(new BasicNameValuePair("pricipal",principal.getText().toString()));
+
+            UrlEncodedFormEntity ent = null;
+            Log.d("abc",params.toString());
+
+            ent = new UrlEncodedFormEntity(params, HTTP.UTF_8);
+            post.setEntity(ent);
+            HttpResponse responsePOST = client.execute(post);
+            HttpEntity resEntity = responsePOST.getEntity();
+
+            if (resEntity != null) {
+                result = EntityUtils.toString(resEntity);
+                Log.d("abcd",result);
+            }
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            client.getConnectionManager().shutdown();
+        }
+    }
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event)
